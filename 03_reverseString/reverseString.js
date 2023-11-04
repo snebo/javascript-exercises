06a5c4d6f6c59b3f6b5e963cae5730686f4f0d2e
Stefano <snebo54@hotmail.com>
2023-11-05 00:12:22 +0100

    Solved the first 3 excersises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newWord=""
+    for (i= word.length-1; i>=0; i--){
+        newWord+= word[i]
+    }
+    return newWord;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
