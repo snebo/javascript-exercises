b9176dc3d746445a594dd086b49f6f6600dcd230
Stefano <snebo54@hotmail.com>
2023-11-05 23:54:44 +0100

    Fix: completed the logic for remaining test conditions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,16 @@
 const findTheOldest = function(people_data) {
-// take the multidimensional arrary
-// itrate through them 
-// sum minus their death from their birth
-// check if it is older than the next iteration
-// return the oldest value
+
     let age=0
     let oldest=0, index=0
     for (let i = 0; i<people_data.length;i++){
         age= people_data[i].yearOfDeath - people_data[i].yearOfBirth
+
+        if (typeof people_data[i].yearOfDeath === "undefined"){
+            let current_Date = new Date()
+            let yearLiving = current_Date.getFullYear()
+            age=yearLiving-people_data[i].yearOfBirth
+        }
+
         if(oldest < age){
             oldest =age
             index =i
