21e69099ea295809886b5aca055ef32800648db6
Stefano <snebo54@hotmail.com>
2023-11-05 20:00:15 +0100

    Added logic for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+// take the string
+// remove spaces
+// reverse the string
+// check if they match
+newPhrase = phrase.replace(" ","")
+reversePhrase = newPhrase.split("").reverse().join("")
+if (newPhrase == reversePhrase){return true}
+else{return false}
 };
 
 // Do not edit below this line
