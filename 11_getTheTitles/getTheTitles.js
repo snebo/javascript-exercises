71008129611d3f169cf7cd0413275c3e799c7603
Stefano <snebo54@hotmail.com>
2023-11-05 23:40:13 +0100

    Solved the first test condition

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(library) {
+let book_section= []
+    for (let i = 0; i<library.length;i++){
+        book_section.push(library[i].title)
+    }
+    return book_section
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people_data) {
+// take the multidimensional arrary
+// itrate through them 
+// sum minus their death from their birth
+// check if it is older than the next iteration
+// return the oldest value
+    let age=0
+    let oldest=0, index=0
+    for (let i = 0; i<people_data.length;i++){
+        age= people_data[i].yearOfDeath - people_data[i].yearOfBirth
+        if(oldest < age){
+            oldest =age
+            index =i
+        } 
+    }
+    return people_data[index]
 };
 
 // Do not edit below this line
