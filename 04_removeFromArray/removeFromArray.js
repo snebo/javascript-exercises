99e3ab1e8c7fd59f738d2e7f87e7e79d84326c32
Stefano <snebo54@hotmail.com>
2023-11-05 17:09:17 +0100

    Removed debug logs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,9 @@
 const removeFromArray = function(Arr, ...args) {
     for(let i = Arr.length-1; i >=0; i--){
         let value=0
-        // loops through the arrary
-        console.log("arrary: "+Arr[i])
 
         while(value < args.length){
-            console.log(args[value]);
             if(Arr[i] === args[value]){
-                console.log("match")
                 Arr.splice(i,1)
             }
             value++
@@ -16,7 +12,6 @@ const removeFromArray = function(Arr, ...args) {
     }
     return Arr
 };
-console.log(removeFromArray([1,2,3,4],2, 1))
 
 // Do not edit below this line
 module.exports = removeFromArray;
