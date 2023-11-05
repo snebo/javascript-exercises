37d087b822146416dca174fae6dc179db6376215
Stefano <snebo54@hotmail.com>
2023-11-05 18:24:09 +0100

    Added extra conditions to satisfy the test cases

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// two conditions for leap leapYears
+// the year is divisible by four
+// years divisible by 100 are only leap years if they are also dviisible by 400
+if (year%100 == 0 && year%400 == 0){
+    return true
+}
+else if(year%100==0 && !(year%400==0)){return false}
+if (year%4==0 ){
+    return true
+}
+else return false;
 };
 
 // Do not edit below this line
