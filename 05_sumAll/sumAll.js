319c647c30cea5565be4316c7b630fa75a02323f
Stefano <snebo54@hotmail.com>
2023-11-05 17:22:27 +0100

    Use basic logic to solve 5th task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(min, n) {
+// take two parameters, min and n
+// create a sum variable
+// while min is < n, we add the min to sum and increament min
+// return sum
+let sum = 0;
+if((typeof min) != "number" || (typeof n) !="number"){
+    return "ERROR"
+}
+else if(min<0 || n<0){return "ERROR"}
+if(min>n){
+    let _ = min
+    min = n
+    n = _
+}
+while(min <= n){
+    sum+=min
+    min++
+}
+return sum
 };
 
 // Do not edit below this line
