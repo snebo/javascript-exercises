c4a98600a57df6e8de17c411f588f75707bc3a27
Stefano <snebo54@hotmail.com>
2023-11-05 19:37:52 +0100

    Added the solutions to question 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum=0
+  for(let i = arr.length-1; i>=0; i--){
+    sum += arr[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(...arg) {
+  let prodcut=0
+  for(let i = arg.length-1; i>=0; i--){
+    prodcut *= arg[i]
+  }
+  return prodcut
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	return number**exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	// formula for factorial n! = n(n-1)(n-2)(n-3)...1
+  let result = 0
+  for (let i = n; i>0; i--){
+    result *=i
+  }
 };
 
 // Do not edit below this line
