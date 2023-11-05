c3abe52790b571a637e4f53429b6e958150b3be4
Stefano <snebo54@hotmail.com>
2023-11-05 19:18:59 +0100

    Solved the 7th question

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  degree = Number(((degree-32)*(5/9)).toFixed(1))
+  return degree
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  degree = Number((degree*(9/5)+32).toFixed(1))
+  return degree
 };
 
 // Do not edit below this line
