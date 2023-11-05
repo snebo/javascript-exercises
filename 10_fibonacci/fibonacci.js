2bcd2fb4f0f0129ad6b0e319fd122974958c782b
Stefano <snebo54@hotmail.com>
2023-11-05 22:42:34 +0100

    Solved the fib logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+// formula for the fibonacci is F(n) = F(n-1)+ F(n-2)
+// create a fib sequence
+// work with recursions or loops?
+//check for negative no
+    if (fib<0) return "OOPS"
+    else if(fib<=1) return Number(fib)
 
+    return fibonacci(fib-1)+fibonacci(fib-2)
+
+    // using while loop
+    // let a =0, b=1
+    // while(b<fib){
+        // a,b = b, a+b
+    // }
+    // return b
 };
 
 // Do not edit below this line
