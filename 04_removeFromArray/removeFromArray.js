707dba3f6d08e661e5371a060505454006410e69
Stefano <snebo54@hotmail.com>
2023-11-05 12:02:34 +0100

    Use splice method to solve problem ISSUES: after splicing, the arrary index is rearranged.this leads to the loop skipping an arrary index.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(Arr, ...args) {
+    for(let i=0; i < Arr.length; i++){
+        let value=0
+        // loops through the arrary
+        console.log("arrary: "+Arr[i])
 
+        while(value < args.length){
+            console.log(args[value]);
+            if(Arr[i] === args[value]){
+                Arr.splice(i, 1)
+                // to fix the skipped index, we check again
+                if(Arr[i] === (args[value])){
+                    Arr.splice(i,1)
+                }
+            }
+            value++
+        }
+        // for(let j=0; j<args.length; j++){
+        //     console.log("value: "+args[j])
+        //     if (Arr[i] === args[j]){
+        //         Arr.splice(i,1)
+        //     }
+        // }
+    }
+    return Arr
 };
+console.log(removeFromArray([1,2,3,4],2, 1))
 
 // Do not edit below this line
 module.exports = removeFromArray;
